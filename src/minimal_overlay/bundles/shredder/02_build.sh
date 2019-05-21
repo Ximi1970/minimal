12c844273790b02e77202d7f0e5a6e27eff657ca
Maxime Rijnders <ximi.github@gmail.com>
2019-05-21 10:37:44 +0200

    Install main script

--- src/minimal_overlay/bundles/shredder/02_build.sh
+++ src/minimal_overlay/bundles/shredder/02_build.sh
@@ -12,5 +12,8 @@ cd $WORK_DIR/overlay/$BUNDLE_NAME
 install -d -m755 "$OVERLAY_ROOTFS/etc"
 install -d -m755 "$OVERLAY_ROOTFS/etc/autorun"
 install -m755 "$SRC_DIR/99_shredder.sh" "$OVERLAY_ROOTFS/etc/autorun/99_shredder.sh"
+install -d -m755 "$OVERLAY_ROOTFS/usr"
+install -d -m755 "$OVERLAY_ROOTFS/usr/bin"
+install -m755 "$SRC_DIR/shredder.sh" "$OVERLAY_ROOTFS/usr/bin/shredder.sh"
 
 echo "Shredder scripts have been installed."
