b7a324ba95eed26ecbd801c35393cc711f79b83f
Ximi1970 <ximi.github@gmail.com>
2019-05-26 22:33:18 +0200

    Fix ncurses dependencies

--- src/minimal_overlay/bundles/nano/02_build.sh
+++ src/minimal_overlay/bundles/nano/02_build.sh
@@ -21,8 +21,9 @@ rm -rf $DEST_DIR
 echo "Configuring '$BUNDLE_NAME'."
 CFLAGS="$CFLAGS" ./configure \
     --prefix=/usr \
-    CFLAGS="-I$WORK_DIR/overlay/ncurses/ncurses_installed/usr/include $CFLAGS" \
-    LDFLAGS="-L$WORK_DIR/overlay/ncurses/ncurses_installed/usr/lib"
+    CFLAGS="-I$WORK_DIR/overlay/ncurses/ncurses_installed/usr/include" \
+    LDFLAGS="-L$WORK_DIR/overlay/ncurses/ncurses_installed/usr/lib" \
+    LIBS="-lz -lncurses -ltinfo"
 
 echo "Building '$BUNDLE_NAME'."
 make -j $NUM_JOBS
